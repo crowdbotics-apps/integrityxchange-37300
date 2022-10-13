1d8f0c5c57e548fc6210b3926e6a5f89f78e0fdf
Crowdbotics <team@crowdbotics.com>
2022-10-13 17:17:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "integrityxchange_37300",
+  url: "https://integrityxchange_37300.botics.co",
+  api: "https://integrityxchange_37300.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
